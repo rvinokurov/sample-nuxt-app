98a101e1fd814c2bd2600dae7d8c8c1529a990e1
Roman Vinokurov <r.vinokurov@tinkoff.ru>
2022-05-21 21:12:38 +0300

    initial

---
+++ pages/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <Tutorial/>
+</template>
+
+
+<script>
+
+
+function getMeta($http) {
+  return $http.$get('http://6289226c7af826e39e690910.mockapi.io/api/v1/meta/1');
+}
+
+export default {
+  data() {
+
+     return {
+       meta: {}
+     }
+  },
+  async fetch() {
+    this.meta = await fetch('https://6289226c7af826e39e690910.mockapi.io/api/v1/meta/1', {
+      https: true
+    }).then(res => res.json());
+  },
+  head() {
+    console.log(this.meta);
+    return {
+      title: this.meta.title,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: this.meta.description
+        }
+      ],
+    }
+
+  }
+}
+</script>
